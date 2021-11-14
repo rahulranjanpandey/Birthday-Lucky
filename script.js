30404fa6b660fa57b69a92de8ca731b1411c4163
Rahul Ranjan Pandey <62777073+rahulranjanpandey@users.noreply.github.com>
2021-11-14 19:18:53 +0530

    commit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkNumberButton = document.querySelector("#check-number-button");
+
+checkNumberButton.addEventListener('click',functiongetValues())
