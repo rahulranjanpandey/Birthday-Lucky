76de191adf575b31c2257c4c206b84cb660e278d
Rahul Ranjan Pandey <62777073+rahulranjanpandey@users.noreply.github.com>
2021-11-14 18:52:33 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumberButton = document.querySelector("#check-number-button");
