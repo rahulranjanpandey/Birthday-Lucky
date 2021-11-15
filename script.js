dccf3478bf2047f9b654c298a061ea3fa2cdc8f4
Rahul Ranjan Pandey <62777073+rahulranjanpandey@users.noreply.github.com>
2021-11-15 22:05:16 +0530

    commit

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
 const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
-const checkNumberButton = document.querySelector("#check-number-button");
+const checkNumberButton = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output");
 
-checkNumberButton.addEventListener('click',functiongetValues())
+function compareValues(sum, luckyNumber) {
+    if(sum%luckyNumber===0) {
+        outputBox.innerText="Yay! Your birthday is lucky 😄";
+    }else {
+        outputBox.innerText="Oh no! Your birthday is not lucky ☹️";
+    }
+}
+
+function checkBirthDateIsLucky() {
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    if (sum&&dob) {
+
+        compareValues(sum, luckyNumber.value);
+    }
+    else {
+
+        outputBox.innerText = "Enter both the values";
+    }
+}
+
+function calculateSum(dob) {
+    dob = dob.replaceAll("-","");
+    let sum = 0;
+    for(let index = 0; index<dob.length; index++) {
+        sum = sum + Number(dob.charAt(index));
+    }
+    return sum;
+}
+
+
+
+
+
+checkNumberButton.addEventListener("click", checkBirthDateIsLucky);
